8580b7832d875805b37c9cbece65854a9604da33
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-11-13 00:26:27 -0400

    contador de vida e pontuação adicionados

--- index.js
+++ index.js
@@ -3,36 +3,42 @@ const pipe = document.querySelector("#pipe");
 const game_over = document.querySelector("#game-over");
 const clouds = document.querySelector("#clouds");
 
-let jumped = false;
+const score = document.querySelector("#score");
+const life_counter = document.querySelector("#life-counter");
 
 const handleKeydown = (event) => {
   if (event.key === " ") {
     mario.classList.add("jump");
-    jumped = true;
 
     setTimeout(() => {
       mario.classList.remove("jump");
-      jumped = false;
     }, 500);
   }
 };
 
 const checkGameOver = () => {
+  let is_scoring = 0;
+
   const pipe_x_position = pipe.offsetLeft;
   const clouds_position = clouds.offsetLeft;
   const mario_y_position = +getComputedStyle(mario).bottom.replace("px", "");
-
   if (
     pipe_x_position <= 123 &&
     pipe_x_position > 0 &&
     mario_y_position <= 100
   ) {
     gameOverAnimation(pipe_x_position, clouds_position);
+  } else if (pipe_x_position === -1) {
+    is_scoring++;
+    score.innerHTML = `${is_scoring}pts`;
   }
   requestAnimationFrame(checkGameOver);
 };
 
 const gameOverAnimation = (pipe_x_position, clouds_position) => {
+  let life = 3;
+  life--;
+
   pipe.style.animation = "none";
   pipe.style.left = `${pipe_x_position}px`;
 
@@ -41,6 +47,8 @@ const gameOverAnimation = (pipe_x_position, clouds_position) => {
 
   clouds.style.animation = "none";
   clouds.style.left = `${clouds_position}px`;
+
+  life_counter.innerHTML = `X${life}`;
 };
 
 checkGameOver();
